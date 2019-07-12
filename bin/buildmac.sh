2b861f90ad4972250b8e4c2af9d96f01c0666ed9
monsterkodi <monsterkodi@gmx.net>
2019-07-12 23:59:07 +0200

    build

--- bin/buildmac.sh
+++ bin/buildmac.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
-cd `dirname $0`/..
+
+DIR=`dirname $0`
+BIN=$DIR/../node_modules/.bin
+cd $DIR/..
 
 if rm -rf kalk-darwin-x64; then
     
-    if node_modules/.bin/konrad; then
+    if $BIN/konrad; then
 
         IGNORE="/(.*\.dmg$|Icon$|watch$|icons$|.*md$|pug$|styl$|.*\.lock$|img/banner\.png)"
     
-        node_modules/.bin/electron-packager . --overwrite --icon=img/app.icns --darwinDarkModeSupport --ignore=$IGNORE
+        if $BIN/electron-packager . --overwrite --icon=img/app.icns --darwinDarkModeSupport --ignore=$IGNORE; then
+            
+            rm -rf /Applications/kalk.app
+            cp -R kalk-darwin-x64/kalk.app /Applications
+            
+            open /Applications/kalk.app 
+        fi
     fi
 fi
--- bin/inst.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd `dirname $0`/..
-
-rm -rf /Applications/kalk.app
-cp -R kalk-darwin-x64/kalk.app /Applications
-
-open /Applications/kalk.app 
